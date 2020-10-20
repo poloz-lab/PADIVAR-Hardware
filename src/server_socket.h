c4efc21725fee89c795959cad8764940d9ee54f3
poloz <polozfr@laposte.net>
2020-10-20 12:41:44 +0200

    doxygen documentation for server_socket.h

--- src/server_socket.h
+++ src/server_socket.h
@@ -1,4 +1,12 @@
 #ifndef SERVER_SOCKET_H
 #define SERVER_SOCKET_H
 
+/*!
+ * \file server_socket.h
+ * \brief socket for accepting connexion from clients
+ * \author poloz-lab
+ * \version 0.1
+ * \date 20/10/2020
+ */
+
 #endif
