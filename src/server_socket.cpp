e125f1288a6b57566e09e95d374ac55a26018906
poloz <polozfr@laposte.net>
2020-10-20 12:44:38 +0200

    doxygen documentation for socket_server.c

--- src/server_socket.cpp
+++ src/server_socket.cpp
@@ -1,3 +1,11 @@
 #include "server_socket.h"
 
+/*!
+ * \file server_socket.c
+ * \brief socket for accpeting connexion from clients
+ * \author poloz-lab
+ * \version 0.1
+ * \date 20/10/2020
+ */
+
 
