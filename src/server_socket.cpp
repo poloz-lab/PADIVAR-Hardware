058b5c785fd4d89f186015b5fa016594cbb30af9
poloz <polozfr@laposte.net>
2020-10-20 14:57:28 +0200

    creation of the socket declaration of the class ServerSocket the constructor creates a socket declaration of ExceptionSocketServerType to handle problems while using the socket

--- src/server_socket.cpp
+++ src/server_socket.cpp
@@ -1,4 +1,5 @@
 #include "server_socket.h"
+#include <string>
 
 /*!
  * \file server_socket.c
@@ -8,4 +9,33 @@
  * \date 20/10/2020
  */
 
+ServerSocket::ServerSocket(unsigned int port)
+{
+    // creation of the socket
+    socket_fd_ = socket(AF_INET, SOCK_STREAM, 0); // create a IPv4 for TCP connection
+    if (socket_fd_ == -1) // if the creation failed
+    {
+        throw ExceptionSocketServer(ExceptionSocketServerTypes::Creation, errno);
+    }
+}
 
+ExceptionSocketServer::ExceptionSocketServer(ExceptionSocketServerType type, int errno_c = 0) throw()
+    :type_(type),errno_(errno_c)
+{
+}
+
+const char *ExceptionSocketServer::what() const throw()
+{
+    std::string reason;
+    switch(type_)
+    {
+        case ExceptionSocketServerTypes::NoError:
+            reason = "no error";
+            break;
+        case ExceptionSocketServerTypes::Creation:
+            reason = "can't create the socket";
+            break;
+    }
+    std::string detailed_reason = reason + " errno: " + std::to_string(errno_);
+    return detailed_reason.c_str();
+}
--- src/server_socket.h
+++ src/server_socket.h
@@ -8,5 +8,68 @@
  * \version 0.1
  * \date 20/10/2020
  */
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <cerrno>
+#include <exception>
+
+/*!
+ * \class ServerSocket
+ * \brief class for the server socket
+ */
+class ServerSocket
+{
+public:
+    /*!
+     * \brief constructor
+     * \param port : network port for listening connections
+     */
+    ServerSocket(unsigned int port);
+private:
+    int socket_fd_; /*< file descriptor for the socket */
+    struct sockaddr_in server_address_; /*< structure for ip, stores accepted addresses, port */
+};
+
+/*!
+ * \namespace
+ */
+namespace ExceptionSocketServerTypes
+{
+    /*!
+     * \enum ExceptionSocketServerType
+     * \brief type of error that could be encountered with the server socket
+     */
+    enum ExceptionSocketServerType
+    {
+        NoError, /*!< no error */
+        Creation, /*!< error while creating the socket */
+    };
+}
+typedef ExceptionSocketServerTypes::ExceptionSocketServerType ExceptionSocketServerType;
+
+/*!
+ * \class ExceptionSocketServer
+ * \brief class for handling errors from the server socket
+ */
+class ExceptionSocketServer: public std::exception
+{
+public:
+    /*!
+     * \brief constructor
+     * \param type : type of the error
+     * \param errno_c : error number (errno) from C lib
+     */
+    ExceptionSocketServer(ExceptionSocketServerType type, int errno_c) throw();
+
+    /*!
+     * \brief give the reason of the exception
+     * \return string to give the reason of the exception
+     */
+    virtual const char* what() const throw();
+private:
+    ExceptionSocketServerType type_;
+    int errno_;
+};
 
 #endif
