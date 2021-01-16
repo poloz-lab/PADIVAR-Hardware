2953b7512839f25c2d06a46c16a8d0ee951cf2bd
poloz <polozfr@laposte.net>
2021-01-16 12:08:23 +0100

    forbid access to default constructor from ServerSocket

--- src/server_socket.h
+++ src/server_socket.h
@@ -172,6 +172,7 @@ public:
      */
     ClientSocket waitingForConnection();
 private:
+    ServerSocket(); // forbid access to default constructor
     int socket_fd_; /*< file descriptor for the socket */
     struct sockaddr_in server_address_; /*< structure for ip, stores accepted addresses, port */
 };
