fa7c807552e547037be67af929ebc9ea4ca1ab1b
poloz <polozfr@laposte.net>
2021-03-10 13:43:26 +0100

    doc bluetooth.cpp

--- src/bluetooth.cpp
+++ src/bluetooth.cpp
@@ -36,6 +36,11 @@ The fact that you are presently reading this means that you have had
 knowledge of the CeCILL license and that you accept its terms.
 */
 
+/*!
+ * \file bluetooth.cpp
+ * \brief handle bluetooth connections
+ */
+
 #include "bluetooth.h"
 #include <list>
 #include <string>
